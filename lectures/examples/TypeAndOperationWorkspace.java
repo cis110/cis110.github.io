eab8f7216b5c02b9980a0d6c9add4abd2133317b
Harry Smith <Harry Smith>
2021-01-21 15:38:24 -0500

    monday's lecture material, too

---
+++ lectures/examples/TypeAndOperationWorkspace.java
@@ -0,0 +1,7 @@
+public class TypeAndOperationWorkspace {
+    public static void main(String[] args) {
+        int fivePlusThree = 5 + 3;
+
+        System.out.println("5 + 3 evaluates to " + fivePlusThree);
+    }
+}
