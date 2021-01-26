a35871ca1ecc8ec89d4e138150adc20cbf03a29b
Harry Smith <Harry Smith>
2021-01-26 12:18:52 -0500

    wednesday's example; remove announcement

---
+++ _site/lectures/examples/DigitReversal.java
@@ -0,0 +1,22 @@
+/**
+ * Name: Harry Smith PennKey: sharry
+ * 
+ * Usage: java DigitReversal
+ * 
+ * Description: Takes a four-digit integer and identifies each of the digits.
+ * Then, the program prints out the number with all digits reversed. We can
+ * determine the value of each digit by repeated application of % and /
+ * (remainder and integer division).
+ * 
+ * If we start with 4893, then thousands : 4, hundreds : 8, tens : 9, ones : 3.
+ * 
+ * Prints out "4893 backwards is 3984"
+ */
+
+public class DigitReversal {
+    public static void main(String[] args) {
+
+        int inputNumber = 4893;
+
+    }
+}
